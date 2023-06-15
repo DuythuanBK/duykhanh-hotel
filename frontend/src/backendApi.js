cf7b3e108f4651381d6d8c32e3e6a762c3ba7edd
Thuan <duythuan.th@gmail.com>
2023-06-15 22:04:00 +0700

    fix frontend

--- frontend/src/backendApi.js
+++ frontend/src/backendApi.js
@@ -34,71 +34,71 @@ const requests = () => {
 }
 
 const Item = {
-  getAll: () => requests().get("/items").then(response => response.json()).catch(error => {}),
-  get: (id) => requests().get(`/items/${id}`).then(response => response.json()).catch(error => {}),
-  update: (item) => requests().put(`/items/${item.id}`, { "item": item }).then(response => { }).catch(error => {}),
-  post: async (item) => requests().post('/items', { "item": item }).then(response => response.json()).catch(error => {}),
-  delete: (id) => requests().del(`/items/${id}`).catch(error => {})
+  getAll: () => requests().get("/items").then(response => response.json()),
+  get: (id) => requests().get(`/items/${id}`).then(response => response.json()),
+  update: (item) => requests().put(`/items/${item.id}`, { "item": item }).then(response => { }),
+  post: async (item) => requests().post('/items', { "item": item }).then(response => response.json()),
+  delete: (id) => requests().del(`/items/${id}`)
 }
 
 const RoomPrice = {
-  getAll: () => requests().get("/room-prices").then(response => response.json()).catch(error => {}),
-  update: (item) => requests().put(`/room-prices/${item.id}`, { "roomPrice": item }).then(response => response.json()).catch(error => {}),
-  post: (item) => requests().post('/room-prices', { "roomPrice": item }).then(response => response.json()).catch(error => {}),
-  delete: (id) => requests().del(`/room-prices/${id}`).catch(error => {})
+  getAll: () => requests().get("/room-prices").then(response => response.json()),
+  update: (item) => requests().put(`/room-prices/${item.id}`, { "roomPrice": item }).then(response => response.json()),
+  post: (item) => requests().post('/room-prices', { "roomPrice": item }).then(response => response.json()),
+  delete: (id) => requests().del(`/room-prices/${id}`)
 }
 
 const RoomType = {
-  getAll: () => requests().get("/room-types").then(response => response.json()).catch(error => {}),
-  update: (item) => requests().put(`/room-types/${item.id}`, { "roomType": item }).then(response => { }).catch(error => {}),
-  post: (item) => requests().post('/room-types', { "roomType": item }).then(response => response.json()).catch(error => {}),
-  delete: (id) => requests().del(`/room-types/${id}`).catch(error => {})
+  getAll: () => requests().get("/room-types").then(response => response.json()),
+  update: (item) => requests().put(`/room-types/${item.id}`, { "roomType": item }).then(response => { }),
+  post: (item) => requests().post('/room-types', { "roomType": item }).then(response => response.json()),
+  delete: (id) => requests().del(`/room-types/${id}`)
 }
 
 const HouseType = {
-  getAll: () => requests().get("/house-types").then(response => response.json()).catch(error => {}),
-  update: (item) => requests().put(`/house-types/${item.id}`, { "houseType": item }).then(response => { }).catch(error => {}),
-  post: (item) => requests().post('/house-types', { "houseType": item }).then(response => response.json()).catch(error => {}),
-  delete: (id) => requests().del(`/house-types/${id}`).catch(error => {})
+  getAll: () => requests().get("/house-types").then(response => response.json()),
+  update: (item) => requests().put(`/house-types/${item.id}`, { "houseType": item }).then(response => { }),
+  post: (item) => requests().post('/house-types', { "houseType": item }).then(response => response.json()),
+  delete: (id) => requests().del(`/house-types/${id}`)
 }
 
 const RoomConfig = {
-  getAll: () => requests().get("/room-configs").then(response => response.json()).catch(error => {}),
-  update: (item) => requests().put(`/room-configs/${item.id}`, { "roomConfig": item }).catch(error => {}),
-  post: (item) => requests().post('/room-configs', { "roomConfig": item }).then(response => response.json()).catch(error => {}),
-  delete: (id) => requests().del(`/room-configs/${id}`).catch(error => {})
+  getAll: () => requests().get("/room-configs").then(response => response.json()),
+  update: (item) => requests().put(`/room-configs/${item.id}`, { "roomConfig": item }),
+  post: (item) => requests().post('/room-configs', { "roomConfig": item }).then(response => response.json()),
+  delete: (id) => requests().del(`/room-configs/${id}`)
 }
 
 const Room = {
-  getAll: () => requests().get("/rooms").then(response => response.json()).catch(error => {}),
-  getRoom: (fullName) => requests().get(`/rooms/${fullName}`).then(response => response.json()).catch(error => {}),
-  update: (fullName, body) => requests().put(`/rooms/${fullName}`, { "room": body }).then(response => response.json()).catch(error => {}),
-  checkIn: (fullName) => requests().post(`/rooms/${fullName}/check-in`).then(response => response.json()).catch(error => {}),
-  checkout: (fullName) => requests().post(`/rooms/${fullName}/check-out`).then(response => response.json()).catch(error => {}),
+  getAll: () => requests().get("/rooms").then(response => response.json()),
+  getRoom: (fullName) => requests().get(`/rooms/${fullName}`).then(response => response.json()),
+  update: (fullName, body) => requests().put(`/rooms/${fullName}`, { "room": body }).then(response => response.json()),
+  checkIn: (fullName) => requests().post(`/rooms/${fullName}/check-in`).then(response => response.json()),
+  checkout: (fullName) => requests().post(`/rooms/${fullName}/check-out`).then(response => response.json()),
 }
 
 const Order = {
-  getAll: (fullName) => requests().get(`/rooms/${fullName}/items`).then(response => response.json()).catch(error => {}),
-  update: (fullName, item) => requests().put(`/rooms/${fullName}/items/${item.id}`, {"item": item}).catch(error => {}),
-  post: (fullName, item) => requests().post(`/rooms/${fullName}/items`, {"item": item}).then(response => response.json()).catch(error => {}),
+  getAll: (fullName) => requests().get(`/rooms/${fullName}/items`).then(response => response.json()),
+  update: (fullName, item) => requests().put(`/rooms/${fullName}/items/${item.id}`, {"item": item}),
+  post: (fullName, item) => requests().post(`/rooms/${fullName}/items`, {"item": item}).then(response => response.json()),
   delete: (fullName, id) => requests().del(`/rooms/${fullName}/items/${id}`)
 }
 
 const Customer = {
-  get: (fullName) => requests().get(`/rooms/${fullName}/customer`).then(response => response.json()).catch(error => {}),
-  update: (fullName, item) => requests().put(`/rooms/${fullName}/customer`, {"customer": item}).catch(error => {}),
-  post: (fullName, item) => requests().post(`/rooms/${fullName}/customer`, {"customer": item}).then(response => response.json()).catch(error => {}),
-  delete: (fullName, id) => requests().del(`/rooms/${fullName}/customer/${id}`).catch(error => {})
+  get: (fullName) => requests().get(`/rooms/${fullName}/customer`).then(response => response.json()),
+  update: (fullName, item) => requests().put(`/rooms/${fullName}/customer`, {"customer": item}),
+  post: (fullName, item) => requests().post(`/rooms/${fullName}/customer`, {"customer": item}).then(response => response.json()),
+  delete: (fullName, id) => requests().del(`/rooms/${fullName}/customer/${id}`)
 }
 
 let limit = (count, p) => `limit=${count}&offset=${p}`;
 
 const RoomHistory = {
-  get: (params, page) => requests().get(`/history/rooms?${limit(10, page)}&houseType=${params.houseType}&date=${params.date}`).then(response => response.json()).catch(error => {})
+  get: (params, page) => requests().get(`/history/rooms?${limit(10, page)}&houseType=${params.houseType}&date=${params.date}`).then(response => response.json())
 }
 
 const OrderHistory = {
-  get: (id) => requests().get(`/history/${id}/orders`).then(res => res.json()).catch(error => {})
+  get: (id) => requests().get(`/history/${id}/orders`).then(res => res.json())
 }
 
 
--- frontend/src/components/operating/Renting.js
+++ frontend/src/components/operating/Renting.js
@@ -22,7 +22,7 @@ function Information({ room, handleStatusChanged, updateRoomInformation, handleC
   return (
     <div className="col-4 overflow-auto" style={{ height: "90vh" }}>
       <div className="row g-3">
-        <RoomInformation room={room} updateRoomInformation={updateRoomInformation} handleCheckoutRoom={handleCheckoutRoom} handleStatusChanged={handleStatusChanged}/>
+        <RoomInformation room={room} updateRoomInformation={updateRoomInformation} handleCheckoutRoom={handleCheckoutRoom} handleStatusChanged={handleStatusChanged} />
         <OrderInformation room={room} handleStatusChanged={handleStatusChanged} />
         <CustomerInformation room={room} />
       </div>
@@ -39,8 +39,8 @@ export default function Renting() {
   let timerIDs = new Map();
 
   useState(() => {
-      backendApi.Room.getAll().then(data => setRoomList(data.rooms)).catch(error => {
-      })
+    backendApi.Room.getAll().then(data => setRoomList(data.rooms)).catch(error => {
+    })
   }, []);
 
   const handleStatusChanged = (newStatus) => {
@@ -61,12 +61,12 @@ export default function Renting() {
     backendApi.Room.checkIn(room.fullName).then(data => {
       const changedRoom = data.room;
       const rooms = roomList.map(r => {
-        if(r.fullName === changedRoom.fullName) {
+        if (r.fullName === changedRoom.fullName) {
           return changedRoom;
         } else {
           return r;
         }
-        
+
       });
       const id = setInterval(updateFeeOfRoom(room.fullName), 900000);
       timerIDs.set(room.fullName, id);
@@ -79,13 +79,11 @@ export default function Renting() {
   const handleCheckoutRoom = (room) => {
     backendApi.Room.checkout(room.fullName).then(data => {
       const rooms = roomList.map(r => {
-        if(r.fullName === data.room.fullName) {
+        if (r.fullName === data.room.fullName) {
           return data.room;
         } else {
           return r;
         }
-      }).catch(error => {
-      
       })
 
       const id = timerIDs.get(room.fullName);
@@ -101,13 +99,11 @@ export default function Renting() {
     backendApi.Room.update(room.fullName, updateRoom).then(data => {
       const room = data.room;
       const newRooms = roomList.map(r => {
-        if(r.fullName === room.fullName) {
+        if (r.fullName === room.fullName) {
           return room;
         }
         return r;
-      }).catch(error => {
-      
-      });
+      })
       setRoomList(newRooms);
     })
   }
@@ -116,7 +112,7 @@ export default function Renting() {
     try {
       const data = await backendApi.Room.getRoom(fullName);
       return data.room;
-    } catch(error) {
+    } catch (error) {
 
     }
   }
@@ -124,7 +120,7 @@ export default function Renting() {
   const updateFeeOfRoom = async (fullName) => {
     const room = await getRoomInformation(fullName);
     const newRooms = roomList.map(r => {
-      if(r.fullName === room.fullName) {
+      if (r.fullName === room.fullName) {
         return room;
       }
       return r;
@@ -136,12 +132,10 @@ export default function Renting() {
     backendApi.Room.update(fullName, body).then(data => {
       const room = data.room;
       const newRooms = roomList.map(r => {
-        if(r.fullName === room.fullName) {
+        if (r.fullName === room.fullName) {
           return room;
         }
         return r;
-      }).catch(error => {
-
       });
       setRoomList(newRooms);
     })
@@ -152,23 +146,23 @@ export default function Renting() {
       <div className="container-fluid">
         <div className="row">
           <div className="col-8">
-          <select className="ms-4 mb-2 fs-4" style={{width: "150px", height: "40px"}} defaultValue='Nhà cũ' onChange={handleChooseHouseTypeChanged}>
-            <option value="Nhà cũ">Nhà cũ</option>
-            <option value="Nhà mới">Nhà mới</option>
-          </select>
-          <div className="container-fluid">
-            <div className="row">
-            {
-              roomList.filter(it => it.houseType === choosedHouseType).map((room, index) => <RoomPreview key={index} status={status} room={room}
-                handleCheckinRoom={handleCheckinRoom}
-                handleChooseRoomChanged={handleChooseRoomChanged}
-                handleStatusChanged={handleStatusChanged}
-                handleCheckoutRoom={handleCheckoutRoom}
-                handleCleanRoomClick={handleCleanRoomClick}
-              />)
-            }
+            <select className="ms-4 mb-2 fs-4" style={{ width: "150px", height: "40px" }} defaultValue='Nhà cũ' onChange={handleChooseHouseTypeChanged}>
+              <option value="Nhà cũ">Nhà cũ</option>
+              <option value="Nhà mới">Nhà mới</option>
+            </select>
+            <div className="container-fluid">
+              <div className="row">
+                {
+                  roomList.filter(it => it.houseType === choosedHouseType).map((room, index) => <RoomPreview key={index} status={status} room={room}
+                    handleCheckinRoom={handleCheckinRoom}
+                    handleChooseRoomChanged={handleChooseRoomChanged}
+                    handleStatusChanged={handleStatusChanged}
+                    handleCheckoutRoom={handleCheckoutRoom}
+                    handleCleanRoomClick={handleCleanRoomClick}
+                  />)
+                }
+              </div>
             </div>
-          </div>
           </div>
           {
             status === RENTING.INFOMATION ?
